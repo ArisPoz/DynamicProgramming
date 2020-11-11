d6b3bfc259a95b6a9cade84689b347d0f168722e
Aristotelis Pozidis <arist.poz@gmail.com>
2020-11-11 01:38:47 +0100

    Some fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,28 @@
+import Part1.File.File;
+import Part1.File.Type.ImageSubType.Png;
+import Part1.File.Type.VideoSubType.Avi;
+import Part1.File.Type.VideoSubType.Mp4;
+import Part1.File.TypeHandler;
+import Part1.Visitor.Visitor;
 import Part2.BST;
 
-public class AspectJMain {
+import java.lang.reflect.Proxy;
+
+public class Main {
     public static void main(String[] args) throws InterruptedException {
+        System.out.println("Part1.Visitor with Dynamic proxy");
+        System.out.println("------------------------------------------------");
+        File file = new Mp4();
+        File file1 = new Avi();
+        File file2 = new Png();
+
+        Class[] interfaces = new Class[1];
+        interfaces[0] = Visitor.class;
+        Visitor proxy = (Visitor) Proxy.newProxyInstance(Main.class.getClassLoader(), interfaces, new TypeHandler());
+        proxy.visit(file);
+        proxy.visit(file1);
+        proxy.visit(file2);
+
         System.out.println("Binary Search Tree with AspectJ for Concurrency addition");
         System.out.println("-----------------------------------------------------------");
         BST.createTestBST();
